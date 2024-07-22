b6fd38b9136d813b003aa7df9190cb7b18726575
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-07-22 14:48:22 +0300

    addcomit2

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,10 +1,17 @@
 import { model, Schema } from 'mongoose';
+import { ROLES } from '../../constants/index.js';
+
 
 const usersSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: [ROLES.TEACHER, ROLES.PARENT],
+      default: ROLES.PARENT,
+    },
   },
   { timestamps: true, versionKey: false },
 );
@@ -14,5 +21,7 @@ usersSchema.methods.toJSON = function () {
   delete obj.password;
   return obj;
 };
+
+
 
 export const UsersCollection = model('users', usersSchema);
--- src/routers/students.js
+++ src/routers/students.js
@@ -13,6 +13,9 @@ import {
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { createStudentSchema, updateStudentSchema } from '../validation/students.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
+import { checkRoles } from '../middlewares/checkRoles.js';
+import { ROLES } from '../constants/index.js';
 
 const router = Router();
 
@@ -50,6 +53,45 @@ router.get(
   '/:studentId',
   isValidId,
   ctrlWrapper(getStudentByIdController),
+);
+
+router.use(authenticate);
+
+router.get('/', ctrlWrapper(getStudentsController));
+
+router.get('/', checkRoles(ROLES.TEACHER), ctrlWrapper(getStudentsController));
+
+router.get(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
+  ctrlWrapper(getStudentByIdController),
+);
+
+router.post(
+  '/',
+  checkRoles(ROLES.TEACHER),
+  validateBody(createStudentSchema),
+  ctrlWrapper(createStudentController),
+);
+
+router.put(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER),
+  validateBody(createStudentSchema),
+  ctrlWrapper(upsertStudentController),
+);
+
+router.patch(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
+  validateBody(updateStudentSchema),
+  ctrlWrapper(patchStudentController),
+);
+
+router.delete(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER),
+  ctrlWrapper(deleteStudentController),
 );
 
 
